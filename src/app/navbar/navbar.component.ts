072f0c4d7bb73d576dfc4a324a75af419c163bbb
Eden Kharoub <51828039+Kokoden@users.noreply.github.com>
2021-11-09 23:12:23 +0100

    feat: add the navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  navLinks: any[] = [
+    { name: 'home', route: '/home' },
+    { name: 'playground', route: '/playground' }
+  ];
+
+  activePage: string = '';
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if(event instanceof NavigationEnd) {
+        this.activePage = event.urlAfterRedirects.split('/')[1];
+      }
+    });
   }
 
 }
